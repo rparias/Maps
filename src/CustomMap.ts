65c3fcf4fefa0e9b35da0604c325a3606e8f9f43
Ronald Arias <ronald.arias@thoughtworks.com>
2021-04-02 20:18:04 -0500

    Adds a marker point in the map with the company location, it contains duplicated code

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -22,6 +22,12 @@ export class CustomMap {
   }
 
   addCompanyMarker(company: Company): void {
-    
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lon
+      }
+    });
   }
 }
