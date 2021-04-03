e8531603cfcbe323675e9c5c05448229bba3d86f
Ronald Arias <ronald.arias@thoughtworks.com>
2021-04-02 20:02:20 -0500

    Creates a CustomMap class and encapsulates the functionality of a map available in the index.ts

---
+++ src/CustomMap.ts
@@ -0,0 +1,10 @@
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(elementId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(elementId), {
+      zoom: 1,
+      center: {lat: 0, lng: 0}
+    });
+  }
+}
